6240bc858ff75055c6579f528aa8bfb6152d6097
ieltswithieis-design <ieltswithieis@gmail.com>
2026-09-25 16:54:42 +0500

    Delete src directory

--- src/data/ieltsData.ts
+++
@@ -1,8 +0,0 @@
-import data from './ieltsData.json';
-import { IeltsDatabase } from '../types/ielts';
-import { readingTests } from './readingTestsData';
-
-export const ieltsDatabase: IeltsDatabase = {
-  ...(data as unknown as IeltsDatabase),
-  reading: readingTests,
-};
